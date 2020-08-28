b4a74aacfdbaeb64534487624c6a429bdb4c629d
vkarakolis <vkarakolis@epu.ntua.gr>
2020-08-28 14:02:21 +0300

    removed db credentials

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,10 @@
 import os
 
-POSTGRES_USER = os.environ.get('POSTGRES_USER', 'admin')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'admin')
-POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'qualichain.epu.ntua.gr')
-POSTGRES_PORT = os.environ.get('POSTGRES_PORT', 5432)
-POSTGRES_DB = os.environ.get('POSTGRES_DB', 'api_db')
+POSTGRES_USER = os.environ.get('POSTGRES_USER', '')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', '')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', '')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT', 0)
+POSTGRES_DB = os.environ.get('POSTGRES_DB', '')
 
 ENGINE_STRING = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
     POSTGRES_USER,
